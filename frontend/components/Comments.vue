a6ff9483fd213cc458ded320acc39ec2d5fe3b07
Steven Pledger <steevstevep@gmail.com>
2020-08-29 14:38:18 -0700

    Add CSS overrides for commento

--- frontend/components/Comments.vue
+++ frontend/components/Comments.vue
@@ -9,7 +9,9 @@ export default {
             {
                 hid: 'commento',
                 src: 'https://commento.tiffanietru.com/js/commento.js',
-                defer: true
+                defer: true,
+                'data-css-override': 'https://tiffanietru.com/css/commento.css',
+                'data-auto-init': true
             },
         ],
     }
